e4d688624b9e524100b93041d26ef4fa62d72421
ApocalyVec <s-vector.lee@hotmail.com>
2019-06-17 00:14:25 -0400

    finish building the RNN and save the trained model

--- rnn.py
+++ rnn.py
@@ -28,8 +28,50 @@ for i in range(60, len(training_set_scaled)):
 x_train, y_train = np.array(x_train), np.array(y_train)
 
 # Reshaping
-x_train = np.reshape(x_train, ())
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 # part 2 - building the RNN
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
 
+# Initialising the RNN
+regressor = Sequential()
+
+# Adding the first LSTM layer and some Dropout regularization
+# input shape = (time steps, number of indicators)
+regressor.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+regressor.add(Dropout(0.2))  # ignore 20% of the neurons in both forward and backward propagation
+
+# Adding a second LSTM layer and some Dropout regularization
+# we don't need to put input_shape here, because:
+# 1. this is not the input layer, 2. the units = 50 from previous layer tells this layer what is its input_shape
+regressor.add(LSTM(units=50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# Adding a third LSM layer and some Dropout regularization
+regressor.add(LSTM(units=50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# Adding a forth LSTM layer and some Dropout regularization
+regressor.add(LSTM(units=50, return_sequences=False))  # or return_sequences is default at False
+regressor.add(Dropout(0.2))
+
+# Adding  the output layer
+regressor.add(Dense(units=1))  # in this case, units is the dimension of the output layer
+
+# Compiling the RNN
+regressor.compile(optimizer='adam', loss='mean_squared_error')
+
+regressor.fit(x_train, y_train, epochs=100, batch_size=32)
+
+# saving the trained model
+from keras.models import load_model
+
+regressor.save('/trained_model/regresspr.h5')
+'''
+use this line if you want to load the model back
+regressor = load_model('/trained_model/regressor.h5')
+'''
 # part3
